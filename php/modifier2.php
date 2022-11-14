814e21f2893806d29154904a3cc493c8c21e23f1
TDIAKITE23 <tiemokodjandiakite@gmail.com>
2022-11-14 16:25:56 +0100

    changement des titles

--- php/modifier2.php
+++ php/modifier2.php
@@ -19,7 +19,7 @@ $res=mysqli_fetch_array($req);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Modif</title>
 </head>
 <body>
     <form action="update2.php" method="post">
